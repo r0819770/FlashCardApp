131dc8001e16e2daf603bfb6db66656e1c0abb31
Jonas De Bie <jonas.debie@student.ucll.be>
2022-11-12 22:13:49 +0100

    first push

---
+++ app/src/main/java/com/example/flashcardapp/ItemCard.java
@@ -0,0 +1,6 @@
+package com.example.flashcardapp;
+
+public class ItemCard {
+    public String id;
+    public String data;
+}
--- app/src/main/java/com/example/flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/flashcardapp/MainActivity.java
@@ -1,14 +1,108 @@
 package com.example.flashcardapp;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+    List<ItemCard> cardList;
+    ListView cardListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void sendMessage(View view) {
+
+
+        Map<String, Object> card = new HashMap<>();
+        card.put("id", "1");
+        card.put("data", "newData");
+
+
+        // Add a new document with a generated ID
+        db.collection("cards")
+                .add(card)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
+        db.collection("cards")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        if (!queryDocumentSnapshots.isEmpty()) {
+                            List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                            for (DocumentSnapshot d : list) {
+                                if (d.toObject(ItemCard.class) != null){
+                                ItemCard i = d.toObject(ItemCard.class);
+
+                                cardList.add(i);
+                                }
+                            }
+                        }
+                    }
+                });
+    }
+
+    public void UpdateCardListView(List<ItemCard> itemCardList){
+
+        cardListView = (ListView) findViewById(R.id.cardListView);
+
+        List<String> your_array_list = new ArrayList<String>();
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                your_array_list);
+
+        for (ItemCard card : itemCardList)
+        {
+            your_array_list.add(card.data);
+        }
+
+        cardListView.setAdapter(arrayAdapter);
+    }
+
+
 }
